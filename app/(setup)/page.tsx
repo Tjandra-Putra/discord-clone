8f0cd797e06eb6b44a06bed30b9eee41d619615c
Tjandra Putra <tjandrap.work@gmail.com>
2023-12-05 00:12:12 +0800

    Create server component

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,5 @@
 // root file
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -22,7 +23,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
